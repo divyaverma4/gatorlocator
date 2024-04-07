ef870bfbf944312596f327affcf4709fa7c259ab
divyaverma4 <147621065+divyaverma4@users.noreply.github.com>
2024-04-06 21:07:34 -0400

    Update main.py
    added activity and match functions

--- main.py
+++ main.py
@@ -14,6 +14,30 @@ def interests_similarity(user1, user2):
     common_interests = set(user1.interest_answers) & set(user2.interest_answers)
     return common_interests
 
+#activity doesn't run unless called by match function
+# basically just spits out possible activities based on similar interests
+def activity(similarities_list):
+    match_activity = []
+    for item in similarities_list:
+        if (item == "books"):
+            match_activity.append("visit the local bookstore!")
+        elif (item == "plants"):
+            match_activity.append("visit the local greenhouse!")
+        elif (item == "anime"):
+            match_activity.append("have an anime watch night!")
+    return match_activity
+
+# takes 2 users, assesses their compatibility
+# if compatible, returns their activity list
+def match(user1, user2):
+    if (personality_similarity(user1, user2) >= 5):
+        similarities_list = interests_similarity(user1, user2)
+        if similarities_list >= 3:
+            return activity(similarities_list)
+        else:
+            return 0
+
+
 # testing:
 user1 = UserProfile("Anna", 19, "Female", "Statistics", [1,2,2,2,1,1,1,1,2,"yes"], ["raves", "anime", "books", "gym", "plants"] )
 user1.save_to_database()
@@ -22,3 +46,9 @@ user2.save_to_database()
 
 print(personality_similarity(user1, user2))
 print(interests_similarity(user1, user2))
+
+user1and2 = match(user1, user2)
+if (user1and2 != 0):
+    print("Congratulations, you've been matched! Together you should: \n")
+    for item in user1and2:
+        print(item + "\n")
